e4509859e4fefffa8f91cd182a3270eb98fc6361
doyun <tkdtlr1998@gmail.com>
2025-12-23 14:19:23 +0900

    :sparkles: [Feat] buzzer control 헤더

---
+++ buzzer/buzzer.h
@@ -0,0 +1,13 @@
+#ifndef BUZZER_H
+#define BUZZER_H
+
+#define MUSIC_SCHOOL_BELL       1
+#define MUSIC_TWINKLE_STAR      2
+#define MUSIC_HAPPY_BIRTHDAY    3
+#define MUSIC_BUTTERFLY         4
+
+int music_init(int speaker_pin);
+void music_cleanup(void);
+int play_music(int music_number);
+
+#endif
